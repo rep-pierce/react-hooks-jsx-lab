36f670090b086f5bc87575de1e7622e5b57b7d3d
Ryan Pierce <pierce.ryan.97@gmail.com>
2022-10-12 10:44:55 -0600

    commit tree 08b5b9d7ce075e5b45f8705a105fee05103a2b63

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+      <h2>About Me</h2>
+      <p>Hi, my name is Ryan Pierce and I am a student at Flatiron School</p>
+      <img src={image} alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
